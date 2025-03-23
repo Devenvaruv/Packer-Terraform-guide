699e47882adc439ade1f15c6f98baabbbf79b9d8
Devenvaruv <devenvaruv@gmail.com>
2025-03-23 13:23:38 -0700

    chore(infra) : added packer and shell script for custom-AMI

---
+++ packer/setup-docker.sh
@@ -0,0 +1,7 @@
+echo "Installing Docker ..."
+sudo yum install -y docker
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
+newgrp docker
+docker --version
